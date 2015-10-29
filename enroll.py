f74488dd925a47ed12c9a863bdb6d0518c7d4394
Hans <cwang25@ncsu.edu>
2015-10-29 00:25:23 -0400

    remove reduny rwjekl;

--- enroll.py
+++ enroll.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class Test(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -14,20 +15,18 @@ class Test(unittest.TestCase):
         self.base_url = "https://portalsp.acs.ncsu.edu/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
     def test_(self):
         driver = self.driver
-        driver.get(self.base_url + "/shibboleth-ds/?entityID=https%3A%2F%2Fportalsp.acs.ncsu.edu%2Fsp%2Fshibboleth&return=https%3A%2F%2Fportalsp.acs.ncsu.edu%2FShibboleth.sso%2FLogin%3FSAMLDS%3D1%26target%3Dss%253Amem%253Abf2250a2f15cb4f69f9b1553bb986eeaa2feb5cad02051e72e12fdf3b5fc7b6c")
+        driver.get(
+            self.base_url + "/shibboleth-ds/?entityID=https%3A%2F%2Fportalsp.acs.ncsu.edu%2Fsp%2Fshibboleth&return=https%3A%2F%2Fportalsp.acs.ncsu.edu%2FShibboleth.sso%2FLogin%3FSAMLDS%3D1%26target%3Dss%253Amem%253Abf2250a2f15cb4f69f9b1553bb986eeaa2feb5cad02051e72e12fdf3b5fc7b6c")
         driver.find_element_by_css_selector("img[alt=\"NCSU Faculty/Staff/Students\"]").click()
         driver.find_element_by_id("j_username").clear()
         driver.find_element_by_id("j_username").send_keys("sdlfkjsdf")
         driver.find_element_by_id("j_password").clear()
         driver.find_element_by_id("j_password").send_keys("fskdjhfskjdfh")
         driver.find_element_by_id("formSubmit").click()
-		
-		
-		
-		
+
         time.sleep(10)
         driver.find_element_by_link_text("FOR STUDENTS").click()
         # ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | _top | 30000]]
@@ -36,28 +35,27 @@ class Test(unittest.TestCase):
         # ERROR: Caught exception [ERROR: Unsupported command [selectFrame |  | ]]
         time.sleep(5)
         driver.get("https://cs9prd.acs.ncsu.edu/psc/CS9PRD/EMPLOYEE/NCSIS/s/WEBLIB_JQUERYUI.ISCRIPT1.FieldFormula.IScript_Enrollment_Wizard")
-        #driver.execute_script('document.getElementById("tabs-2-tab").click()')
-        #driver._switch_to.frame(driver.find_elements_by_tag_name("iframe")[1])
-        #elem = driver.find_element(By.XPATH, '//html/body/form/div[4]/table/tbody/tr/td/div/table/tbody/tr[2]/td[2]/div/div/iframe')
-        #driver._switch_to.frame(driver.find_element_by_xpath("/html/body/form/div[4]/table/tbody/tr/td/div/table/tbody/tr[2]/td[2]/div/div/iframe"))
-        #driver._switch_to.frame(elem)
         time.sleep(10)
         driver.find_element_by_id("tabs-2-tab").click()
-        #driver.find_element_by_id("tabs-2-tab").click()
-        #driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/ul/li[2]/a').click()
+        # driver.find_element_by_id("tabs-2-tab").click()
+        # driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/ul/li[2]/a').click()
         # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
         driver.find_element_by_link_text("SIGN OUT").click()
-    
+
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException, e:
+            return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -67,11 +65,13 @@ class Test(unittest.TestCase):
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
-    
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
-        #self.driver.quit()
+        # self.driver.quit()
         self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
